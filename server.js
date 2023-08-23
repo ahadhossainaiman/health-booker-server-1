4c0ee78be84f4e971f384d82b5cc33b7b7e3fd17
ahadhossainaiman <ahadhossainaiman017@gmail.com>
2023-08-24 04:21:28 +0600

    c

--- server.js
+++ server.js
@@ -25,9 +25,9 @@ app.use("/api/appointment", appointRouter);
 app.use("/api/notification", notificationRouter);
 app.use(express.static(path.join(__dirname, "./client/build")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// });
 
 app.get("/", (req, res) => {
   res.json("Hello");
