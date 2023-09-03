204330b2b445f0cc15a8fc27833c6b728ddaf672
Ahad Hossain Aiman <66985311+ahadhossainaiman@users.noreply.github.com>
2023-09-04 02:05:52 +0600

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: ["https://unique-kataifi-484bfc.netlify.app"],
+    origin: ["https://64f4deaff0d5a743b890012b--musical-horse-81f0e5.netlify.app/"],
     methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS", "PUT"],
     credentials: true,
   })
